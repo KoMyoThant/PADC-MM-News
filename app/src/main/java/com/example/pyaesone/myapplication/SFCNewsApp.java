fbd1f38b6a98fc2ab07a136aafff085856e4af96
Pyae Sone <myothantktt@gmail.com>
2017-11-04 17:49:01 +0630

    Initialize skeleton project with default structure / value.

---
+++ app/src/main/java/com/example/pyaesone/myapplication/SFCNewsApp.java
@@ -0,0 +1,17 @@
+package com.example.pyaesone.myapplication;
+
+import android.app.Application;
+
+/**
+ * Created by pyaesone on 11/4/17.
+ */
+
+public class SFCNewsApp extends Application {
+
+    public static final String LOG_TAG = "SFCNewsApp";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
